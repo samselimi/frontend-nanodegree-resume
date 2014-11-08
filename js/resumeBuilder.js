714fd29de33baa81a02e326a96abdc01febe9e7c
samselimi <samselimi@hotmail.com>
2014-11-07 23:21:22 -0600

    added pics

---
+++ js/resumeBuilder.js
@@ -0,0 +1,42 @@
+//var bio = {
+//  "thatname" : "Sam Selimovski",
+//  "age" : 36,
+//  "city" : "Chicago"
+//}
+//bio["location"] = "Chicago";
+//bio.school = "Pace University";
+
+
+var work = {
+    "jobs": [
+        {
+            "employer": "ATT",
+            "title": "Technology Manager",
+            "dates": "2006 - 2014",
+            "description": "all technology related work"
+        },
+        {
+            "employer": "ATT",
+            "title": "Network Manager",
+            "dates": "2014 - Current",
+            "description": "Lead group of field technicians"
+        }
+    ]
+};
+
+var projects = {
+    "projects": [
+        {
+            "title": "service promise checklist",
+            "dates": "2013",
+            "description": "create and deploy service promise checklist",
+            "images": "images/me.jpg"
+        },
+        {
+            "employer": "ATT",
+            "title": "Network Manager",
+            "dates": "2014 - Current",
+            "description": "Lead group of field technicians"
+        }
+    ]
+}
